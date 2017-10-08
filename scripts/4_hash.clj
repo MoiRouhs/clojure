116d34d1761c91308f908454e3b369f23be57c2c
Carlos Rocha <MoiRouhs@users.noreply.github.com>
2017-10-08 13:54:07 -0500

    Hash básico

---
+++ scripts/4_hash.clj
@@ -0,0 +1,8 @@
+;; Hash son estructuras parecidas a los de ruby y se componen de una llave y su valor.
+
+(def df {:a "amor", :b "bobo", :c "coco"}) ;; los hash puede tener claves ":clave" como en ruby
+(def palabras {"amor" 1, "dios" 5, "amigo" 20}) ;; La llave y el valor se separa por n espacio, y las parajeas por una coma 
+
+(println (map? palabras)) ;; Se le pregunta si es un hash devuelve "true / false"
+(println (get df :a)) ;; Se obtiene el valor de una llave.
+(println (palabras "amigo")) ;; Otra forma de obtenerlo sin usar "get"
