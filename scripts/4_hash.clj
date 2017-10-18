d18c2bfa1be5604b2a1512a68b2600f118b10a83
Carlos Rocha <MoiRouhs@users.noreply.github.com>
2017-10-17 19:10:01 -0500

    Hash actualización

--- scripts/4_hash.clj
+++ scripts/4_hash.clj
@@ -4,5 +4,7 @@
 (def palabras {"amor" 1, "dios" 5, "amigo" 20}) ;; La llave y el valor se separa por n espacio, y las parajeas por una coma 
 
 (println (map? palabras)) ;; Se le pregunta si es un hash devuelve "true / false"
+(println (keys palabras)) ;; Mostrar las llaves del Hash
+(println (vals palabras)) ;; Mostrar los valores del Hash
 (println (get df :a)) ;; Se obtiene el valor de una llave.
 (println (palabras "amigo")) ;; Otra forma de obtenerlo sin usar "get"
